b90c47ac45b4e4e0f11ea02bc4c67f114b790187
Linard Arquint <linard.arquint@inf.ethz.ch>
2020-10-08 11:31:29 +0200

    switches from actions/nightly-release to viperproject/create-nightly-release

--- .github/actions/nightly-release/src/Constants.ts
+++
@@ -1,6 +0,0 @@
-export default class Constants {
-    public static get INVISIBLE_BODY_PREAMBLE(): string {
-        return "<!-- Automatically created nightly release: "
-            + "This release can safely be deleted by the nightly-release action -->";
-    }
-}
