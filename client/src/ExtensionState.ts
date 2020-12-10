16a985eec37bf0e2fb12faad0df33b16322651e6
Linard Arquint <linard.arquint@inf.ethz.ch>
2020-12-10 08:28:56 +0100

    adds client side modifications to start and connect to a server or simply connect to an already running server

--- client/src/ExtensionState.ts
+++ client/src/ExtensionState.ts
@@ -4,13 +4,13 @@
 //
 // Copyright (c) 2011-2020 ETH Zurich.
 
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, ServerOptions, StreamInfo } from 'vscode-languageclient';
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as path from 'path';
 import * as net from 'net';
 import * as child_process from "child_process";
-import { FileData, VerifierConfig, GobraSettings } from "./MessagePayloads";
+import * as readline from 'readline';
+import { FileData, VerifierConfig } from "./MessagePayloads";
 import { Helper, FileSchemes } from "./Helper";
 import { IdeEvents } from "./IdeEvents";
 import { Verifier } from "./VerificationService";
@@ -90,7 +90,15 @@ export class State {
     // const prefix = __dirname.split("client")[0];
     // const serverBin = path.join(prefix, 'server', 'target', 'scala-2.12', 'server.jar')
 
-    let serverOptions: ServerOptions = () => State.startServerProcess(serverBin);
+    const serverOptions: ServerOptions = () => State.startServerProcess(serverBin);
+    // use the following lines to connect to a server instance instead of starting a new one (e.g. for debugging purposes)
+    /*
+    const connectionInfo = {
+      host: "localhost",
+      port: 8080
+    }
+    const serverOptions: ServerOptions = () => State.connectToServer(connectionInfo);
+    */
 
     // server binary was not found
     if (!fs.existsSync(serverBin)) {
@@ -119,50 +127,82 @@ export class State {
     return State.client.onReady();
   }
 
-
   // creates a server for the given server binary
-  private static async startServerProcess(serverBin: string): Promise<any> {
-    // test whether java and z3 binaries can be used:
-    Helper.log("Checking Java...");
-    const javaPath = await Helper.getJavaPath();
-    await Helper.spawn(javaPath, ["--version"]);
-    Helper.log("Checking Z3...");
-    const z3Path = Helper.getZ3Path(Helper.isNightly());
-    await Helper.spawn(z3Path, ["--version"]);
+  private static async startServerProcess(serverBin: string): Promise<StreamInfo> {
+    const javaPath = await State.checkDependenciesAndGetJavaPath();
 
-    return new Promise((resolve, reject) => {
-      let server = net.createServer((socket) => {
-        resolve({reader: socket, writer: socket});
-    
-        // event listener for socket reacting to end
-        socket.on('end', () => Helper.log("Disconnected"));
-    
-      }).on('error', (err) => {
-        const msg = `Error while connecting to Gobra server: '${err}'`
+    // spawn Gobra Server and get port number on which it is reachable:
+    const portNr = await new Promise((resolve:(port: number) => void, reject) => {
+      const portRegex = /<GobraServerPort:(\d+)>/;
+      let portFound: boolean = false;
+      function stdOutLineHandler(line: string): void {
+        // check whether `line` contains the port number
+        if (!portFound) {
+          const match = line.match(portRegex);
+          if (match != null && match[1] != null) {
+            const port = Number(match[1]);
+            if (port != NaN) {
+              portFound = true;
+              resolve(port);
+            }
+          }
+        }
+      }
+
+      const processArgs = ['-Xss128m', '-jar', serverBin/*, '--nThreads', '5'*/];
+      Helper.log(`Gobra IDE: Running '${javaPath} ${processArgs.join(' ')}'`);
+      const serverProcess = child_process.spawn(javaPath, processArgs);
+      // redirect stdout to readline which nicely combines and splits lines
+      const rl = readline.createInterface({ input: serverProcess.stdout });
+      rl.on('line', stdOutLineHandler);
+      serverProcess.stdout.on('data', (data) => Helper.logServer(data));
+      serverProcess.stderr.on('data', (data) => Helper.logServer(data));
+      serverProcess.on('close', (code) => {
+        Helper.log(`Gobra Server process has ended with return code ${code}`);
+      });
+      serverProcess.on('error', (err) => {
+        const msg = `Gorba Server process has encountered an error: ${err}`
         Helper.log(msg);
         reject(msg);
       });
-
-      // start Gobra Server given in binary
-      Helper.log(`Starting Gobra Server with binary ${serverBin}`);
-      server.listen(() => {
-        let serverAddress = server.address() as net.AddressInfo;
-        let processArgs = ['-Xss128m', '-jar', serverBin, serverAddress.port.toString()];
-        let serverProcess = child_process.spawn(javaPath, processArgs);
-
-        serverProcess.stdout.on('data', (data) => Helper.logServer(data));
-        serverProcess.stderr.on('data', (data) => Helper.logServer(data));
-        
-        serverProcess.on('close', (code) => {
-          Helper.log(`Gobra Server process has ended with return code ${code}`);
-        });
-        serverProcess.on('error', (err) => {
-          const msg = `Gorba Server process has encountered an error: ${err}`
-          Helper.log(msg);
-          reject(msg);
-        });
-      })
     });
+
+    // connect to server
+    return new Promise((resolve: (info: StreamInfo) => void, reject) => {
+      const clientSocket = new net.Socket();
+      clientSocket.connect(portNr, 'localhost', () => {
+        Helper.log(`Connected to Gobra Server`);
+        resolve({
+          reader: clientSocket,
+          writer: clientSocket
+        });
+      });
+      clientSocket.on('error', (err) => {
+        Helper.log(`Error occurred on connection to Gobra Server: ${err}`);
+        reject(err);
+      });
+    });
+  }
+
+  // creates a server for the given server binary
+  private static async connectToServer(connectionInfo: net.NetConnectOpts): Promise<StreamInfo> {
+    await State.checkDependenciesAndGetJavaPath();
+    const socket = net.connect(connectionInfo);
+    return {
+      reader: socket,
+      writer: socket
+    };
+  }
+
+  private static async checkDependenciesAndGetJavaPath(): Promise<string> {
+    // test whether java and z3 binaries can be used:
+    Helper.log("Checking Java...");
+    const javaPath = await Helper.getJavaPath();
+    await Helper.spawn(javaPath, ["-version"]);
+    Helper.log("Checking Z3...");
+    const z3Path = Helper.getZ3Path(Helper.isNightly());
+    await Helper.spawn(z3Path, ["--version"]);
+    return javaPath;
   }
 
   public static disposeServer(): Thenable<void> {
