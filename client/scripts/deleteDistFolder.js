0641492d65476b8056ef2d5917fcee4623639583
Linard Arquint <linard.arquint@inf.ethz.ch>
2021-03-09 20:49:20 +0100

    implements a small script to remove the the 'client/dist' folder that also works on Windows

--- client/scripts/deleteDistFolder.js
+++ client/scripts/deleteDistFolder.js
@@ -1,1 +1,9 @@
-console.log("deleteDistFolder called");
+const fs = require('fs');
+
+// this script assumes that it is invoked in the parent folder, i.e. 'client'
+const dir = 'dist';
+try {
+    fs.rmSync(dir, { recursive: true, force: true });
+} catch (err) {
+    console.error(`Error while deleting folder '${dir}'`, err);
+}
